5a9be9c6ea8375dae53e27376ec346e97e27c75b
Oxymels <oxymels@gmail.com>
2015-01-14 01:51:42 +0700

    secret

---
+++ secret.js
@@ -0,0 +1,1 @@
+$("body").prepend('<div class="bandos" style="position: fixed; left: 0; top: 0; background-color: #000; width: 100%; height: 100%; font-size: 128px; text-align: center; color: white; z-index: 9999;">Дарова Бандос</div>');
